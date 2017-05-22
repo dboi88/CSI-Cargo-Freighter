9fabe43b47eae41eadad9a990cec551b0413beef
dboi88 <danielfay88@gmail.com>
2017-05-23 00:33:44 +0100

    Nightly Update
    Added most of load balancing feature, just need to add in onvessel
    trigger to avoid bugs, then build in autostrut module into this as an
    optional feature

---
+++ SOURCE/CSI Tools/Tools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSITools
+{
+    class Tools
+    {
+        /// <summary>
+        /// Name of the Assembly that is running this 
+        /// </summary>
+        internal static String _AssemblyName
+        { get { return System.Reflection.Assembly.GetExecutingAssembly().GetName().Name; } }
+        /// <summary>
+        /// Some Structured logging to the debug file
+        /// </summary>
+        /// <param name="Message">Text to be printed - can be formatted as per String.format</param>
+        /// <param name="strParams">Objects to feed into a String.format</param>
+        internal static void LogFormatted(String Message, params object[] strParams)
+        {
+            Message = String.Format(Message, strParams);                  // This fills the params into the message
+            String strMessageLine = String.Format("{0},{2},{1}",
+                DateTime.Now, Message,
+                _AssemblyName);                                           // This adds our standardised wrapper to each line
+            UnityEngine.Debug.Log(strMessageLine);                        // And this puts it in the log
+        }
+    }
+}
