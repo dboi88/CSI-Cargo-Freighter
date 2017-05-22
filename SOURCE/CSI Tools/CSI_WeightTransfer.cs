cb8722c0518fc26637891b8da67d22d722e20d3d
Daniel Fay <danielfay88@gmail.com>
2017-05-22 12:17:37 +0100

    Update CSI_WeightTransfer.cs

--- SOURCE/CSI Tools/CSI_WeightTransfer.cs
+++ SOURCE/CSI Tools/CSI_WeightTransfer.cs
@@ -10,12 +10,14 @@ namespace CSITools
         [KSPField(isPersistant = true)]
         public int _childcount;
         private bool _trussattached = false;
-
+        
+        ///Set part physics significane Full
         public override void OnStart(StartState state)
         {
             part.physicalSignificance = Part.PhysicalSignificance.FULL;
         }
-
+        
+        ///check if part count changed, if changed check if truss attached and run 'WeightTransfer'
         public void Update()
         {
             var c = part.children.Count;
@@ -26,13 +28,14 @@ namespace CSITools
             _trussattached = IsTrussAttached();
             WeightTransfer();
         }
-
+        
+        /// check if truss module is present on attached parts
         private bool IsTrussAttached()
         {
             return false;
         }
 
-
+        ///Set physics significance depending on presence of rear truss module (CSI_Truss)
         private void WeightTransfer()
         {
             if (_trussattached)
